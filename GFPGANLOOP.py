15f516f45ee5c94df2b8bc898769d50a694d06b8
xzuyn <draox@hotmail.com>
2023-01-16 13:24:44 -0500

    black 79 length

--- GFPGANLOOP.py
+++ GFPGANLOOP.py
@@ -12,6 +12,8 @@ MODELinput = ""
 
 MODELinput = input("Model (1, 1.2, 1.3, or 1.4) [Default: " + MODEL + "]: ")
 
+tic = time.perf_counter()
+
 if MODELinput == "":
     print("Using default model: " + MODEL)
 
@@ -25,27 +27,41 @@ if MODELinput != "":
         print("Invalid model. Using default model: " + MODEL)
         MODELinput = ""
 
-
-tic = time.perf_counter()
-
 # Step 1. 2x GFPGAN.
 ticSTEP1 = time.perf_counter()
 os.system(
-    "python inference_gfpgan.py -i LOOPINPUT -o LOOPTEMP\\LOOPRESULT-2x_" + MODEL + " -v " + MODEL + " -s " + FIRSTRUNSCALE
+    "python inference_gfpgan.py -i LOOPINPUT -o LOOPTEMP\\LOOPRESULT-2x_"
+    + MODEL
+    + " -v "
+    + MODEL
+    + " -s "
+    + FIRSTRUNSCALE
 )
 tocSTEP1 = time.perf_counter()
 
 # Step 2. 2x-1x GFPGAN.
 ticSTEP2 = time.perf_counter()
 os.system(
-    "python inference_gfpgan.py -i LOOPTEMP\\LOOPRESULT-2x_" + MODEL + "\\restored_imgs -o LOOPTEMP\\LOOPRESULT-2x-1x_" + MODEL + " -v " + MODEL + " -s 1"
+    "python inference_gfpgan.py -i LOOPTEMP\\LOOPRESULT-2x_"
+    + MODEL
+    + "\\restored_imgs -o LOOPTEMP\\LOOPRESULT-2x-1x_"
+    + MODEL
+    + " -v "
+    + MODEL
+    + " -s 1"
 )
 tocSTEP2 = time.perf_counter()
 
 # Step 3. 2x-1x-1x GFPGAN.
 ticSTEP3 = time.perf_counter()
 os.system(
-    "python inference_gfpgan.py -i LOOPTEMP\\LOOPRESULT-2x-1x_" + MODEL + "\\restored_imgs -o LOOPTEMP\\LOOPRESULT-2x-1x-1x_" + MODEL + " -v " + MODEL + " -s 1"
+    "python inference_gfpgan.py -i LOOPTEMP\\LOOPRESULT-2x-1x_"
+    + MODEL
+    + "\\restored_imgs -o LOOPTEMP\\LOOPRESULT-2x-1x-1x_"
+    + MODEL
+    + " -v "
+    + MODEL
+    + " -s 1"
 )
 tocSTEP3 = time.perf_counter()
 
@@ -54,13 +70,21 @@ if not os.path.exists("LOOPRESULT"):
     os.makedirs("LOOPRESULT")
 
 
-for RESULTIMAGE in os.listdir("LOOPTEMP\LOOPRESULT-2x-1x-1x_" + MODEL + "\\restored_imgs"):
+for RESULTIMAGE in os.listdir(
+    "LOOPTEMP\LOOPRESULT-2x-1x-1x_" + MODEL + "\\restored_imgs"
+):
     current_name, current_extension = os.path.splitext(RESULTIMAGE)
     shutil.move(
-        ".\LOOPTEMP\LOOPRESULT-2x-1x-1x_" + MODEL + "\\restored_imgs\\"
+        ".\LOOPTEMP\LOOPRESULT-2x-1x-1x_"
+        + MODEL
+        + "\\restored_imgs\\"
         + current_name
         + current_extension,
-        ".\LOOPRESULT\\" + current_name + "-2x-1x-1x_" + MODEL + current_extension,
+        ".\LOOPRESULT\\"
+        + current_name
+        + "-2x-1x-1x_"
+        + MODEL
+        + current_extension,
     )
     PROCESSED_IMAGES.append(str(current_name) + str(current_extension))
     FINAL_IMAGES.append(
