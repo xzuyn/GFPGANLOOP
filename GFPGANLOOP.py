fd0fbdd6a26fcacf95fa2b3e035d4fc92a859330
xzuyn <draox@hotmail.com>
2023-01-16 15:33:33 -0500

    Update GFPGANLOOP.py

--- GFPGANLOOP.py
+++ GFPGANLOOP.py
@@ -9,6 +9,7 @@ MODELinput = ""
 ACCEPTED_MODELS = ["1", "1.2", "1.3", "1.4"]
 FIRSTSCALEFACTOR = "2"
 FSFinput = ""
+FOUNDFACES = False
 
 
 def isFloat(x):
@@ -19,13 +20,14 @@ def isFloat(x):
         return False
 
 
+# Step 0: User input.
 MODELinput = input(f"Model (1, 1.2, 1.3, or 1.4) [Default: {MODEL}]: ")
 FSFinput = input(f"First scaling factor [Default: {FIRSTSCALEFACTOR}]: ")
-
-
-# Step 0. If statements.
-tic = time.perf_counter()
 print()
+
+
+# Step 0.5: If statements.
+tic = time.perf_counter()
 if MODELinput == "":
     print(f"Using default model: {MODEL}")
     print()
@@ -38,7 +40,8 @@ if MODELinput != "":
         MODELinput = ""
 
     else:
-        print(f"Invalid model. Using default model: {MODEL}")
+        print(f"Invalid model.")
+        print(f"Using default model: {MODEL}")
         print()
         MODELinput = ""
 
@@ -65,15 +68,14 @@ if FSFinput != "":
         FSFinput = ""
 
     else:
-        print(
-            f"Invalid scaling factor. Using default first scaling factor: {FIRSTSCALEFACTOR}"
-        )
+        print("Invalid scaling factor")
+        print(f"Using default first scaling factor: {FIRSTSCALEFACTOR}")
         print()
         FSFinput = ""
 
-# Step 1. 2x GFPGAN.
+
+# Step 1: {}x GFPGAN.
 ticSTEP1 = time.perf_counter()
-print()
 print()
 print(f"Step 1: {FIRSTSCALEFACTOR}x GFPGAN using {MODEL} model.")
 os.system(
@@ -81,69 +83,84 @@ os.system(
 )
 tocSTEP1 = time.perf_counter()
 
-# Step 2. 2-1x GFPGAN.
-ticSTEP2 = time.perf_counter()
-print()
-print()
-print(f"Step 2: {FIRSTSCALEFACTOR}-1x GFPGAN using {MODEL} model.")
-os.system(
-    f"python inference_gfpgan.py -i LOOPTEMP\\LOOPRESULT_{FIRSTSCALEFACTOR}x_{MODEL}\\restored_imgs -o LOOPTEMP\\LOOPRESULT_{FIRSTSCALEFACTOR}-1x_{MODEL} -v {MODEL} -s 1"
-)
-tocSTEP2 = time.perf_counter()
+# Check if a face has been found in any image from the first step.
+if len(os.listdir(f"LOOPTEMP\\LOOPRESULT_{FIRSTSCALEFACTOR}x_{MODEL}")) == 0:
+    FOUNDFACES = False
+    print()
+    print("No faces found in any images.")
+    print()
 
-# Step 3. 2-1-1x GFPGAN.
-ticSTEP3 = time.perf_counter()
-print()
-print()
-print(f"Step 3: {FIRSTSCALEFACTOR}-1-1x GFPGAN using {MODEL} model.")
-os.system(
-    f"python inference_gfpgan.py -i LOOPTEMP\\LOOPRESULT_{FIRSTSCALEFACTOR}-1x_{MODEL}\\restored_imgs -o LOOPTEMP\\LOOPRESULT_{FIRSTSCALEFACTOR}-1-1x_{MODEL} -v {MODEL} -s 1"
-)
-tocSTEP3 = time.perf_counter()
-
-if not os.path.exists("LOOPRESULT"):
-    os.makedirs("LOOPRESULT")
-
-for RESULTIMAGE in os.listdir(
-    f"LOOPTEMP\LOOPRESULT_{FIRSTSCALEFACTOR}-1-1x_{MODEL}\\restored_imgs"
+if (
+    not len(os.listdir(f"LOOPTEMP\\LOOPRESULT_{FIRSTSCALEFACTOR}x_{MODEL}"))
+    == 0
 ):
-    current_name, current_extension = os.path.splitext(RESULTIMAGE)
-    shutil.move(
-        f".\LOOPTEMP\LOOPRESULT_{FIRSTSCALEFACTOR}-1-1x_{MODEL}\\restored_imgs\\{current_name}{current_extension}",
-        f".\LOOPRESULT\\{current_name}_{FIRSTSCALEFACTOR}-1-1x_{MODEL}{current_extension}",
+    # Step 2. {}-1x GFPGAN.
+    ticSTEP2 = time.perf_counter()
+    FOUNDFACES = True
+    print()
+    print()
+    print(f"Step 2: {FIRSTSCALEFACTOR}-1x GFPGAN using {MODEL} model.")
+    os.system(
+        f"python inference_gfpgan.py -i LOOPTEMP\\LOOPRESULT_{FIRSTSCALEFACTOR}x_{MODEL}\\restored_imgs -o LOOPTEMP\\LOOPRESULT_{FIRSTSCALEFACTOR}-1x_{MODEL} -v {MODEL} -s 1"
     )
-    PROCESSED_IMAGES.append(str(current_name) + str(current_extension))
-    FINAL_IMAGES.append(
-        str(current_name)
-        + (f"_{FIRSTSCALEFACTOR}-1-1x_{MODEL}")
-        + str(current_extension)
+    tocSTEP2 = time.perf_counter()
+
+    # Step 3. {}-1-1x GFPGAN.
+    ticSTEP3 = time.perf_counter()
+    print()
+    print()
+    print(f"Step 3: {FIRSTSCALEFACTOR}-1-1x GFPGAN using {MODEL} model.")
+    os.system(
+        f"python inference_gfpgan.py -i LOOPTEMP\\LOOPRESULT_{FIRSTSCALEFACTOR}-1x_{MODEL}\\restored_imgs -o LOOPTEMP\\LOOPRESULT_{FIRSTSCALEFACTOR}-1-1x_{MODEL} -v {MODEL} -s 1"
     )
+
+    if not os.path.exists("LOOPRESULT"):
+        os.makedirs("LOOPRESULT")
+    tocSTEP3 = time.perf_counter()
+
+    for RESULTIMAGE in os.listdir(
+        f"LOOPTEMP\LOOPRESULT_{FIRSTSCALEFACTOR}-1-1x_{MODEL}\\restored_imgs"
+    ):
+        current_name, current_extension = os.path.splitext(RESULTIMAGE)
+        shutil.move(
+            f".\LOOPTEMP\LOOPRESULT_{FIRSTSCALEFACTOR}-1-1x_{MODEL}\\restored_imgs\\{current_name}{current_extension}",
+            f".\LOOPRESULT\\{current_name}_{FIRSTSCALEFACTOR}-1-1x_{MODEL}{current_extension}",
+        )
+        PROCESSED_IMAGES.append(str(current_name) + str(current_extension))
+        FINAL_IMAGES.append(
+            str(current_name)
+            + (f"_{FIRSTSCALEFACTOR}-1-1x_{MODEL}")
+            + str(current_extension)
+        )
+
 
 shutil.rmtree(".\LOOPTEMP")
 
-print("------")
-print()
-print()
-print("Processed Images: ")
+if FOUNDFACES is True:
+    print("--------------------------------")
+    print()
+    print()
+    print("Processed Images: ")
 
-for P_IMGS in PROCESSED_IMAGES:
-    print(f"LOOPINPUT\\{P_IMGS}")
+    for P_IMGS in PROCESSED_IMAGES:
+        print(f"LOOPINPUT\\{P_IMGS}")
 
-print()
-print("Final Images: ")
-for F_IMGS in FINAL_IMAGES:
-    print(f"LOOPRESULT\\{F_IMGS}")
+    print()
+    print("Final Images: ")
+    for F_IMGS in FINAL_IMAGES:
+        print(f"LOOPRESULT\\{F_IMGS}")
 
-print()
-print(
-    f"Step 1: {FIRSTSCALEFACTOR}x completed in {tocSTEP1 - ticSTEP1:0.4f} seconds."
-)
-print(
-    f"Step 2: {FIRSTSCALEFACTOR}-1x completed in {tocSTEP2 - ticSTEP2:0.4f} seconds."
-)
-print(
-    f"Step 3: {FIRSTSCALEFACTOR}-1-1x completed in {tocSTEP3 - ticSTEP3:0.4f} seconds."
-)
+    print()
+    print(
+        f"Step 1: {FIRSTSCALEFACTOR}x completed in {tocSTEP1 - ticSTEP1:0.4f} seconds."
+    )
+    print(
+        f"Step 2: {FIRSTSCALEFACTOR}-1x completed in {tocSTEP2 - ticSTEP2:0.4f} seconds."
+    )
+    print(
+        f"Step 3: {FIRSTSCALEFACTOR}-1-1x completed in {tocSTEP3 - ticSTEP3:0.4f} seconds."
+    )
+
 
 toc = time.perf_counter()
 print(f"Entire process completed in {toc - tic:0.4f} seconds.")
