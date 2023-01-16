ab6d9d7e51876234aa200b3ada8b3e2b53cdca56
xzuyn <draox@hotmail.com>
2023-01-16 15:43:32 -0500

    Update GFPGANLOOP.py

--- GFPGANLOOP.py
+++ GFPGANLOOP.py
@@ -9,6 +9,7 @@ MODELinput = ""
 ACCEPTED_MODELS = ["1", "1.2", "1.3", "1.4"]
 FIRSTSCALEFACTOR = "2"
 FSFinput = ""
+LOOPINPUT = "LOOPINPUT"
 FOUNDFACES = False
 
 
@@ -20,147 +21,157 @@ def isFloat(x):
         return False
 
 
-# Step 0: User input.
-MODELinput = input(f"Model (1, 1.2, 1.3, or 1.4) [Default: {MODEL}]: ")
-FSFinput = input(f"First scaling factor [Default: {FIRSTSCALEFACTOR}]: ")
-print()
-
-
-# Step 0.5: If statements.
-tic = time.perf_counter()
-if MODELinput == "":
-    print(f"Using default model: {MODEL}")
+# Step 0: Check if input folder has images.
+if len(os.listdir(f"{LOOPINPUT}")) == 0:
+    print()
+    print("No images in input folder.")
     print()
 
-if MODELinput != "":
-    if MODELinput in ACCEPTED_MODELS:
-        MODEL = MODELinput
-        print(f"Using model: {MODEL}")
-        print()
-        MODELinput = ""
+if not len(os.listdir(f"{LOOPINPUT}")) == 0:
+    # Step 0.25: Ask user what model and first scaling factor they want.
+    MODELinput = input(f"Model (1, 1.2, 1.3, or 1.4) [Default: {MODEL}]: ")
+    FSFinput = input(f"First scaling factor [Default: {FIRSTSCALEFACTOR}]: ")
+    print()
 
-    else:
-        print(f"Invalid model.")
+    # Step 0.5: If statements.
+    tic = time.perf_counter()
+    if MODELinput == "":
         print(f"Using default model: {MODEL}")
         print()
-        MODELinput = ""
 
-if FSFinput == "":
-    print(f"Using default first scaling factor: {FIRSTSCALEFACTOR}")
-    print()
+    if MODELinput != "":
+        if MODELinput in ACCEPTED_MODELS:
+            MODEL = MODELinput
+            print(f"Using model: {MODEL}")
+            print()
+            MODELinput = ""
 
-if FSFinput != "":
-    if FSFinput.isdigit():
-        FIRSTSCALEFACTOR = str(FSFinput)
-        print(f"Using first scaling factor: {FIRSTSCALEFACTOR}")
-        print()
-        FSFinput = ""
+        else:
+            print(f"Invalid model.")
+            print(f"Using default model: {MODEL}")
+            print()
+            MODELinput = ""
 
-    elif isFloat(FSFinput) == True:
-        print(
-            f"Invalid scaling factor. Cannot be a float value. Converting to integer."
-        )
-        print(f"Converted Float: {float(FSFinput)} to {int(float(FSFinput))}")
-
-        FIRSTSCALEFACTOR = str(int(float(FSFinput)))
-        print(f"Using first scaling factor: {FIRSTSCALEFACTOR}")
-        print()
-        FSFinput = ""
-
-    else:
-        print("Invalid scaling factor")
+    if FSFinput == "":
         print(f"Using default first scaling factor: {FIRSTSCALEFACTOR}")
         print()
-        FSFinput = ""
 
+    if FSFinput != "":
+        if FSFinput.isdigit():
+            FIRSTSCALEFACTOR = str(FSFinput)
+            print(f"Using first scaling factor: {FIRSTSCALEFACTOR}")
+            print()
+            FSFinput = ""
 
-# Step 1: {}x GFPGAN.
-ticSTEP1 = time.perf_counter()
-print()
-print(f"Step 1: {FIRSTSCALEFACTOR}x GFPGAN using {MODEL} model.")
-os.system(
-    f"python inference_gfpgan.py -i LOOPINPUT -o LOOPTEMP\\LOOPRESULT_{FIRSTSCALEFACTOR}x_{MODEL} -v {MODEL} -s {FIRSTSCALEFACTOR}"
-)
-tocSTEP1 = time.perf_counter()
+        elif isFloat(FSFinput) == True:
+            print(
+                f"Invalid scaling factor. Cannot be a float value. Converting to integer."
+            )
+            print(
+                f"Converted Float: {float(FSFinput)} to {int(float(FSFinput))}"
+            )
 
-# Check if a face has been found in any image from the first step.
-if len(os.listdir(f"LOOPTEMP\\LOOPRESULT_{FIRSTSCALEFACTOR}x_{MODEL}")) == 0:
-    FOUNDFACES = False
-    print()
-    print("No faces found in any images.")
-    print()
+            FIRSTSCALEFACTOR = str(int(float(FSFinput)))
+            print(f"Using first scaling factor: {FIRSTSCALEFACTOR}")
+            print()
+            FSFinput = ""
 
-if (
-    not len(os.listdir(f"LOOPTEMP\\LOOPRESULT_{FIRSTSCALEFACTOR}x_{MODEL}"))
-    == 0
-):
-    # Step 2. {}-1x GFPGAN.
-    ticSTEP2 = time.perf_counter()
-    FOUNDFACES = True
+        else:
+            print("Invalid scaling factor")
+            print(f"Using default first scaling factor: {FIRSTSCALEFACTOR}")
+            print()
+            FSFinput = ""
+
+    # Step 1: {}x GFPGAN.
+    ticSTEP1 = time.perf_counter()
     print()
-    print()
-    print(f"Step 2: {FIRSTSCALEFACTOR}-1x GFPGAN using {MODEL} model.")
+    print(f"Step 1: {FIRSTSCALEFACTOR}x GFPGAN using {MODEL} model.")
     os.system(
-        f"python inference_gfpgan.py -i LOOPTEMP\\LOOPRESULT_{FIRSTSCALEFACTOR}x_{MODEL}\\restored_imgs -o LOOPTEMP\\LOOPRESULT_{FIRSTSCALEFACTOR}-1x_{MODEL} -v {MODEL} -s 1"
+        f"python inference_gfpgan.py -i {LOOPINPUT} -o LOOPTEMP\\LOOPRESULT_{FIRSTSCALEFACTOR}x_{MODEL} -v {MODEL} -s {FIRSTSCALEFACTOR}"
     )
-    tocSTEP2 = time.perf_counter()
+    tocSTEP1 = time.perf_counter()
 
-    # Step 3. {}-1-1x GFPGAN.
-    ticSTEP3 = time.perf_counter()
-    print()
-    print()
-    print(f"Step 3: {FIRSTSCALEFACTOR}-1-1x GFPGAN using {MODEL} model.")
-    os.system(
-        f"python inference_gfpgan.py -i LOOPTEMP\\LOOPRESULT_{FIRSTSCALEFACTOR}-1x_{MODEL}\\restored_imgs -o LOOPTEMP\\LOOPRESULT_{FIRSTSCALEFACTOR}-1-1x_{MODEL} -v {MODEL} -s 1"
-    )
-
-    if not os.path.exists("LOOPRESULT"):
-        os.makedirs("LOOPRESULT")
-    tocSTEP3 = time.perf_counter()
-
-    for RESULTIMAGE in os.listdir(
-        f"LOOPTEMP\LOOPRESULT_{FIRSTSCALEFACTOR}-1-1x_{MODEL}\\restored_imgs"
+    # Check if a face has been found in any image from the first step.
+    if (
+        len(os.listdir(f"LOOPTEMP\\LOOPRESULT_{FIRSTSCALEFACTOR}x_{MODEL}"))
+        == 0
     ):
-        current_name, current_extension = os.path.splitext(RESULTIMAGE)
-        shutil.move(
-            f".\LOOPTEMP\LOOPRESULT_{FIRSTSCALEFACTOR}-1-1x_{MODEL}\\restored_imgs\\{current_name}{current_extension}",
-            f".\LOOPRESULT\\{current_name}_{FIRSTSCALEFACTOR}-1-1x_{MODEL}{current_extension}",
+        FOUNDFACES = False
+        print()
+        print("No faces found in any images.")
+        print()
+
+    if (
+        not len(
+            os.listdir(f"LOOPTEMP\\LOOPRESULT_{FIRSTSCALEFACTOR}x_{MODEL}")
         )
-        PROCESSED_IMAGES.append(str(current_name) + str(current_extension))
-        FINAL_IMAGES.append(
-            str(current_name)
-            + (f"_{FIRSTSCALEFACTOR}-1-1x_{MODEL}")
-            + str(current_extension)
+        == 0
+    ):
+        # Step 2. {}-1x GFPGAN.
+        ticSTEP2 = time.perf_counter()
+        FOUNDFACES = True
+        print()
+        print()
+        print(f"Step 2: {FIRSTSCALEFACTOR}-1x GFPGAN using {MODEL} model.")
+        os.system(
+            f"python inference_gfpgan.py -i LOOPTEMP\\LOOPRESULT_{FIRSTSCALEFACTOR}x_{MODEL}\\restored_imgs -o LOOPTEMP\\LOOPRESULT_{FIRSTSCALEFACTOR}-1x_{MODEL} -v {MODEL} -s 1"
+        )
+        tocSTEP2 = time.perf_counter()
+
+        # Step 3. {}-1-1x GFPGAN.
+        ticSTEP3 = time.perf_counter()
+        print()
+        print()
+        print(f"Step 3: {FIRSTSCALEFACTOR}-1-1x GFPGAN using {MODEL} model.")
+        os.system(
+            f"python inference_gfpgan.py -i LOOPTEMP\\LOOPRESULT_{FIRSTSCALEFACTOR}-1x_{MODEL}\\restored_imgs -o LOOPTEMP\\LOOPRESULT_{FIRSTSCALEFACTOR}-1-1x_{MODEL} -v {MODEL} -s 1"
         )
 
+        if not os.path.exists("LOOPRESULT"):
+            os.makedirs("LOOPRESULT")
+        tocSTEP3 = time.perf_counter()
 
-shutil.rmtree(".\LOOPTEMP")
+        for RESULTIMAGE in os.listdir(
+            f"LOOPTEMP\LOOPRESULT_{FIRSTSCALEFACTOR}-1-1x_{MODEL}\\restored_imgs"
+        ):
+            current_name, current_extension = os.path.splitext(RESULTIMAGE)
+            shutil.move(
+                f".\LOOPTEMP\LOOPRESULT_{FIRSTSCALEFACTOR}-1-1x_{MODEL}\\restored_imgs\\{current_name}{current_extension}",
+                f".\LOOPRESULT\\{current_name}_{FIRSTSCALEFACTOR}-1-1x_{MODEL}{current_extension}",
+            )
+            PROCESSED_IMAGES.append(str(current_name) + str(current_extension))
+            FINAL_IMAGES.append(
+                str(current_name)
+                + (f"_{FIRSTSCALEFACTOR}-1-1x_{MODEL}")
+                + str(current_extension)
+            )
 
-if FOUNDFACES is True:
-    print("--------------------------------")
-    print()
-    print()
-    print("Processed Images: ")
+    shutil.rmtree(".\LOOPTEMP")
 
-    for P_IMGS in PROCESSED_IMAGES:
-        print(f"LOOPINPUT\\{P_IMGS}")
+    if FOUNDFACES is True:
+        print("--------------------------------")
+        print()
+        print()
+        print("Processed Images: ")
 
-    print()
-    print("Final Images: ")
-    for F_IMGS in FINAL_IMAGES:
-        print(f"LOOPRESULT\\{F_IMGS}")
+        for P_IMGS in PROCESSED_IMAGES:
+            print(f"{LOOPINPUT}\\{P_IMGS}")
 
-    print()
-    print(
-        f"Step 1: {FIRSTSCALEFACTOR}x completed in {tocSTEP1 - ticSTEP1:0.4f} seconds."
-    )
-    print(
-        f"Step 2: {FIRSTSCALEFACTOR}-1x completed in {tocSTEP2 - ticSTEP2:0.4f} seconds."
-    )
-    print(
-        f"Step 3: {FIRSTSCALEFACTOR}-1-1x completed in {tocSTEP3 - ticSTEP3:0.4f} seconds."
-    )
+        print()
+        print("Final Images: ")
+        for F_IMGS in FINAL_IMAGES:
+            print(f"LOOPRESULT\\{F_IMGS}")
 
+        print()
+        print(
+            f"Step 1: {FIRSTSCALEFACTOR}x completed in {tocSTEP1 - ticSTEP1:0.4f} seconds."
+        )
+        print(
+            f"Step 2: {FIRSTSCALEFACTOR}-1x completed in {tocSTEP2 - ticSTEP2:0.4f} seconds."
+        )
+        print(
+            f"Step 3: {FIRSTSCALEFACTOR}-1-1x completed in {tocSTEP3 - ticSTEP3:0.4f} seconds."
+        )
 
-toc = time.perf_counter()
-print(f"Entire process completed in {toc - tic:0.4f} seconds.")
+    toc = time.perf_counter()
+    print(f"Entire process completed in {toc - tic:0.4f} seconds.")
